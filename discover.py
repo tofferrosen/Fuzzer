8aead7599d344022641acbd9b37777770931714f
tofferrosen <cbr4830@rit.edu>
2014-02-16 16:18:36 -0500

    Added link discovery function in discover module

--- discover.py
+++ discover.py
@@ -12,6 +12,9 @@ def page_discovery(page):
 	"""
 	logger.info("Crawling for pages")
 	urls = link_discovery(page)
+
+	#for url in urls:
+		#print url
 	
 
 def link_discovery(page):
@@ -20,7 +23,16 @@ def link_discovery(page):
 	given a page. Returns a list of urls found
 	"""
 	urls = []
-	#print page.text
+	soup = BeautifulSoup(page.content)
+
+	for link in soup.findAll('a'):
+		href = link.get('href')
+
+		# Only include those in our domain and not seen yet
+		if "http://" not in href and href not in urls: 
+			urls.append(link.get('href'))
+
+	return urls
 
 
 
